28fab18baf1c94df170577e6bedabb7c2027090f
krishnoit <krishnoit100@gmail.com>
2023-02-20 05:15:57 +0530

    :fire: FEAT: coustom hooks and responsiveness added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Roboto } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Nav_items from "@/components/landing_page/link";
 import Image from "next/image";
+
+import { useIsMedium } from "@/hooks/customQuery";
 const barlow = Roboto({
   weight: "700",
   subsets: ["latin"],
@@ -14,21 +16,22 @@ const barlow2 = Roboto({
 });
 
 export default function Home() {
+  const medium = useIsMedium()
   return (
     <>
-      <nav className="flex justify-between items-center mx-24 my-12">
-        <div>
-          <motion.img
-          whileTap={{scale:1.7}}
-            animate={{ x: 100 }}
+      <nav className="md:flex md:justify-between md:items-center xl:mx-32 mx-12 md:mx-8 my-12">
+        <div className="">
+          {medium?<motion.img
+            whileTap={{scale:1.7}}
+            initial={{ x: 1000 }}
+            animate={{ x: 0 }}
             transition={{ duration: 1 }}
-            height={100}
-            width={100}
             src="/logo.png"
-          ></motion.img>
+            className="h-16 w-20"
+          ></motion.img>:<></>}
         </div>
-        <div className="flex justify-center gap-12">
-          <motion.div animate={{ x: [300, 0] }} transition={{ duration: 1 }}>
+        <div className="flex justify-center ">
+          <motion.div animate={{ x: [400, 0] }} transition={{ duration: 1 }}>
             <div>
               <a
                 href="#"
@@ -42,7 +45,7 @@ export default function Home() {
               </a>
             </div>
           </motion.div>
-          <motion.div animate={{ x: [200, 0] }} transition={{ duration: 1 }}>
+          <motion.div animate={{ x: [300, 0] }} transition={{ duration: 1 }}>
             <div>
               <a
                 href="#"
@@ -56,13 +59,13 @@ export default function Home() {
               </a>
             </div>
           </motion.div>
-          <motion.div animate={{ x: [100, 0] }} transition={{ duration: 1 }}>
+          <motion.div animate={{ x: [200, 0] }} transition={{ duration: 1 }}>
             <div>
               <a
                 href="#"
                 className={`${styles.cta2} mt-12 flex text-xs items-center`}
               >
-                <span>Blogs</span>
+                <span>Contact</span>
                 <svg width="13px" height="10px" viewBox="0 0 13 10">
                   <path d="M1,5 L11,5"></path>
                   <polyline points="8 1 12 5 8 9"></polyline>
@@ -77,13 +80,16 @@ export default function Home() {
          hero section started
 
       */}
-      <div className="flex justify-between items-center mx-64 mt-32">
-        <div>
+      <div className="md:flex md:justify-between md:items-center xl:mx-64 xl:my-32 md:mx-12 md:my-12">
+        <motion.div initial={{ y: 1000 }}
+                animate={{ y: [1000, 0] }}
+                transition={{ duration: 1.6 }}
+                className="m-4">
           <h1 className={`${barlow.className} text-4xl`}>
             Love to craft <br></br>
             scalable Web app
           </h1>
-          <h1 className={`text-gray-landing text-lg mt-4 mb-12`}>
+          <h1 className={`text-gray-landing md:text-lg text-md mt-4 mb-12`}>
             Hi I'm Krishna, a passionate Full stack developer.<br></br>
             Based in the India
           </h1>
@@ -105,7 +111,7 @@ export default function Home() {
               <a>Reddit</a>
             </div>
           </div>
-        </div>
+        </motion.div>
         <div>
           <div className="flex">
             <div>
