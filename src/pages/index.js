519981cf75f881fa3019a2fe183dbbaf9ceb777c
krishnoit <krishnoit100@gmail.com>
2023-02-20 05:47:59 +0530

    :sparkles: CRTICAL: fix mobile issue !!!!

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         </div>
         {!small ? (
           <div className="flex justify-center ">
-            <div >
+            <div>
               <div>
                 <a
                   href="#"
@@ -52,7 +52,7 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div >
+            <div>
               <div>
                 <a
                   href="#"
@@ -66,7 +66,7 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div >
+            <div>
               <div>
                 <a
                   href="#"
@@ -127,8 +127,6 @@ export default function Home() {
             </motion.div>
           </div>
         )}
-
-       
       </nav>
 
       {/* 
@@ -136,104 +134,105 @@ export default function Home() {
 
       */}
 
-      {small?(
+      {small ? (
         <div className="md:flex md:justify-between md:items-center xl:mx-64 xl:my-32 md:mx-12 md:my-12">
-        <motion.div
-          initial={{ y: 1000 }}
-          animate={{ y: [1000, 0] }}
-          transition={{ duration: 1.6 }}
-          className="m-4"
-        >
-          <h1 className={`${barlow.className} text-4xl`}>
-            Love to craft <br></br>
-            scalable Web app
-          </h1>
-          <h1 className={`text-gray-landing md:text-lg text-md mt-4 mb-12`}>
-            Hi I'm Krishna, a passionate Full stack developer.<br></br>
-            Based in the India
-          </h1>
-          <div>
-            <a href="#" className={`${styles.cta} mt-12 flex items-center`}>
-              <span>See all my work</span>
-              <svg width="13px" height="10px" viewBox="0 0 13 10">
-                <path d="M1,5 L11,5"></path>
-                <polyline points="8 1 12 5 8 9"></polyline>
-              </svg>
-            </a>
-          </div>
-          <div>
-            <div className="flex text-gray-landing gap-8 mt-12">
-              <a>Github</a>
-              <h1>/</h1>
-              <a>Twitter</a>
-              <h1>/</h1>
-              <a>Reddit</a>
-            </div>
-          </div>
-        </motion.div>
-        <div>
-          <div className="flex">
+          <motion.div
+            initial={{ y: 1000 }}
+            animate={{ y: [1000, 0] }}
+            transition={{ duration: 1.6 }}
+            className="m-4"
+          >
+            <h1 className={`${barlow.className} text-4xl`}>
+              Love to craft <br></br>
+              scalable Web app
+            </h1>
+            <h1 className={`text-gray-landing md:text-lg text-md mt-4 mb-12`}>
+              Hi I'm Krishna, a passionate Full stack developer.<br></br>
+              Based in the India
+            </h1>
             <div>
-              <motion.img
-                initial={{ x: 1000 }}
-                animate={{ x: [1000, 0] }}
-                transition={{ duration: 1.6 }}
-                className={styles.imge}
-                src="/tuxpi.com.1676797479.jpg"
-              ></motion.img>
+              <a href="#" className={`${styles.cta} mt-12 flex items-center`}>
+                <span>See all my work</span>
+                <svg width="13px" height="10px" viewBox="0 0 13 10">
+                  <path d="M1,5 L11,5"></path>
+                  <polyline points="8 1 12 5 8 9"></polyline>
+                </svg>
+              </a>
+            </div>
+            <div>
+              <div className="flex text-gray-landing gap-8 mt-12">
+                <a>Github</a>
+                <h1>/</h1>
+                <a>Twitter</a>
+                <h1>/</h1>
+                <a>Reddit</a>
+              </div>
+            </div>
+          </motion.div>
+          <div>
+            <div className="flex">
+              <div>
+                <motion.img
+                  initial={{ x: 1000 }}
+                  animate={{ x: [1000, 0] }}
+                  transition={{ duration: 1.6 }}
+                  className={styles.imge}
+                  src="/tuxpi.com.1676797479.jpg"
+                ></motion.img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      ):( <div className="md:flex md:justify-between md:items-center xl:mx-64 xl:my-32 md:mx-12 md:my-12">
-      <div
-        initial={{ y: 1000 }}
-        animate={{ y: [1000, 0] }}
-        transition={{ duration: 1.6 }}
-        className="m-4"
-      >
-        <h1 className={`${barlow.className} text-4xl`}>
-          Love to craft <br></br>
-          scalable Web app
-        </h1>
-        <h1 className={`text-gray-landing md:text-lg text-md mt-4 mb-12`}>
-          Hi I'm Krishna, a passionate Full stack developer.<br></br>
-          Based in the India
-        </h1>
-        <div>
-          <a href="#" className={`${styles.cta} mt-12 flex items-center`}>
-            <span>See all my work</span>
-            <svg width="13px" height="10px" viewBox="0 0 13 10">
-              <path d="M1,5 L11,5"></path>
-              <polyline points="8 1 12 5 8 9"></polyline>
-            </svg>
-          </a>
-        </div>
-        <div>
-          <div className="flex text-gray-landing gap-8 mt-12">
-            <a>Github</a>
-            <h1>/</h1>
-            <a>Twitter</a>
-            <h1>/</h1>
-            <a>Reddit</a>
+      ) : (
+        <div className="md:flex md:justify-between md:items-center xl:mx-64 xl:my-32 md:mx-12 md:my-12">
+          <div
+            initial={{ y: 1000 }}
+            animate={{ y: [1000, 0] }}
+            transition={{ duration: 1.6 }}
+            className="m-4"
+          >
+            <h1 className={`${barlow.className} text-4xl`}>
+              Love to craft <br></br>
+              scalable Web app
+            </h1>
+            <h1 className={`text-gray-landing md:text-lg text-md mt-4 mb-12`}>
+              Hi I'm Krishna, a passionate Full stack developer.<br></br>
+              Based in the India
+            </h1>
+            <div>
+              <a href="#" className={`${styles.cta} mt-12 flex items-center`}>
+                <span>See all my work</span>
+                <svg width="13px" height="10px" viewBox="0 0 13 10">
+                  <path d="M1,5 L11,5"></path>
+                  <polyline points="8 1 12 5 8 9"></polyline>
+                </svg>
+              </a>
+            </div>
+            <div>
+              <div className="flex text-gray-landing gap-8 mt-12">
+                <a>Github</a>
+                <h1>/</h1>
+                <a>Twitter</a>
+                <h1>/</h1>
+                <a>Reddit</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div className="flex">
           <div>
-            <img
-              initial={{ x: 1000 }}
-              animate={{ x: [1000, 0] }}
-              transition={{ duration: 1.6 }}
-              className={styles.imge}
-              src="/tuxpi.com.1676797479.jpg"
-            ></img>
+            <div className="flex">
+              <div>
+                <img
+                  initial={{ x: 1000 }}
+                  animate={{ x: [1000, 0] }}
+                  transition={{ duration: 1.6 }}
+                  className={styles.imge}
+                  src="/tuxpi.com.1676797479.jpg"
+                ></img>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>)}
-      
+      )}
     </>
   );
 }
