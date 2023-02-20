bf8b71d5b43dac1f4d1113aafcfa40624a254d83
krishnoit <krishnoit100@gmail.com>
2023-02-20 06:25:31 +0530

    :green_heart: IMPROVE : code improved by factor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,20 @@
 import Head from "next/head";
-import { motion } from "framer-motion";
 
-import styles from "@/styles/Home.module.css";
-import Nav_items from "@/components/landing_page/Navbar";
-import Image from "next/image";
-
-
-import { useIsSmall } from "@/hooks/customQuery";
 import Navbar from "@/components/landing_page/Navbar";
 import Hero from "@/components/landing_page/Hero";
 
-
 export default function Home() {
-
   const small = useIsSmall();
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
       {/* 
          hero section started
 
       */}
 
-     <Hero/>
+      <Hero />
     </>
   );
 }
