721334921be5e0b2989e607c44f8ae56405d8598
krishnoit <krishnoit100@gmail.com>
2023-02-20 06:26:37 +0530

    :recycle: REFACTOR : code done

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/landing_page/Navbar";
 import Hero from "@/components/landing_page/Hero";
 
 export default function Home() {
-  const small = useIsSmall();
   return (
     <>
       <Navbar />
