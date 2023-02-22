4287441c60e09b5b216d5b3b9168efbd9696d072
krishnoit <krishnoit100@gmail.com>
2023-02-22 09:41:29 +0530

    :fire: FEAT : almost mobile responsive

--- src/components/landing_page/header.js
+++ src/components/landing_page/header.js
@@ -11,14 +11,14 @@ const barlow = Roboto({
 export default function Header() {
   return (
     <header 
-    className={`${barlow2.className} object-contain selection:text-black-landing selection:bg-white-landing text-7xl flex justify-between items-center  text-white-landing h-screen  overflow-hidden`}>
-      <div className=" relative z-30 p-5 text-white ml-12 rounded-xl">
+    className={`${barlow2.className} object-contain selection:text-black-landing selection:bg-white-landing text-6xl flex justify-between items-center  text-white-landing h-screen  overflow-hidden`}>
+      <div className=" relative z-30 p-2 text-white md:ml-12 rounded-xl">
        Man is still <br></br>the most <br></br>extraordinary <br></br>computer<br></br> of all
-       <h1 className={`${barlow2.className} mr-12 text-lg mt-12`}>
+       <h1 className={`${barlow2.className} md:mr-12 text-lg mt-12`}>
         Jhon F kenndey
        </h1>
       </div>
-      <div className="relative z-30 p-5 text-white mr-24 rounded-xl">
+      <div className="hidden md:block relative z-30 p-5 text-white mr-24 rounded-xl">
        Bibliophile<br></br>
        Cinephile<br></br>
        Coder<br></br>
